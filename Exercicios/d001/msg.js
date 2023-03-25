49ee0e25d44e56e908654f3950d4e2ff0aba25a4
Daniel Cursi <105112847+danielcursi@users.noreply.github.com>
2023-03-25 15:04:43 -0300

    Exercícios curso em vídeo
    Praticando o conhecimento adquirido

---
+++ Exercicios/d001/msg.js
@@ -0,0 +1,11 @@
+function msg1(){
+    window.alert('Você clicou no 1º botão!')
+}
+
+function msg2(){
+    window.alert('Você clicou no 2º botão!')
+}
+
+function msg3(){
+    window.alert('Você clicou no 3º botão!')
+}
---
+++ Exercicios/d002/msg.js
@@ -0,0 +1,5 @@
+function boasvindas(){
+    var nome = window.prompt('Qual o seu nome?')
+    var idade = window.prompt(`Olá, ${nome}! Quantos anos você tem?`)
+    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
+}
