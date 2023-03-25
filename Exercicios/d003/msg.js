ba3b7840812f7e4f29214a5efb685f17663a3b50
Daniel Cursi <105112847+danielcursi@users.noreply.github.com>
2023-03-25 15:20:37 -0300

    Exercicio

---
+++ Exercicios/d003/msg.js
@@ -0,0 +1,5 @@
+function AntSuc(){
+    var n = Number(window.prompt('Digite um número inteiro qualquer'))
+    window.alert(`Antes de ${n}, temos o número ${n - 1} \nDepois de ${n}, temos o número ${n + 1}`)
+
+}
